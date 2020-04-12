22729d0127a64d4d52e1b2879011c472a4e65031
Alojzij Blatnik <lojze.blatnik@gmail.com>
2020-04-12 15:37:59 +0200

    test artifact name

--- builddir/entrypoint.sh
+++ builddir/entrypoint.sh
@@ -11,5 +11,15 @@ zip -r artifact.zip output
 echo "Hello $*"
 time=$(date)
 time2=$(date +%s)
+
+
+
+
+
 echo ::set-output name=time::$time
 echo ::set-output name=time2::$time2
+
+#GITHUB_REF=refs/heads/<branchname>
+echo ::set-output name=gitbranchname::${GITHUB_REF:11}
+echo ::set-output name=gitsha::${GITHUB_SHA}
+
